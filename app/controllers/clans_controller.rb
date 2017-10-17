aa5190632578b624227ff1651ab9c55b9ebe9897
JasonLazzuri <jason.Lazzuri@gmail.com>
2017-10-16 19:20:02 -0700

    fix routes, add gems to gem file, change api key

--- app/controllers/clans_controller.rb
+++ app/controllers/clans_controller.rb
@@ -4,11 +4,11 @@ class ClansController < ApplicationController
 
     if params['clanInput'].nil?
         @input = params['playerInput']
+        @response = RestClient.get 'https://api.clashofclans.com/v1/player/%23'+ @input, {:Authorization => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjU0NzI3MzRiLWEzOTMtNDFlNy05M2ZjLTJkYzE2YmMxMzc0MCIsImlhdCI6MTUwNzkyNDA3Niwic3ViIjoiZGV2ZWxvcGVyLzBmODMwMDgxLTAwZDUtYzdkMC1iODVjLTAxMWFmYzcyZmE2MSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjI0LjE3LjE1LjE2OCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.LJCzaGzmV8pmXdPk4jFEH_3SUUzSVKERnIHCxD8SArNpdtHg5qPEL526j_DluBEGE_WedPLF2g0LG8DmYD6aeg', :content_type => :json}
       else
         @input = params['clanInput']
+        @response = RestClient.get 'https://api.clashofclans.com/v1/clans/%23'+ @input, {:Authorization => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjU0NzI3MzRiLWEzOTMtNDFlNy05M2ZjLTJkYzE2YmMxMzc0MCIsImlhdCI6MTUwNzkyNDA3Niwic3ViIjoiZGV2ZWxvcGVyLzBmODMwMDgxLTAwZDUtYzdkMC1iODVjLTAxMWFmYzcyZmE2MSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjI0LjE3LjE1LjE2OCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.LJCzaGzmV8pmXdPk4jFEH_3SUUzSVKERnIHCxD8SArNpdtHg5qPEL526j_DluBEGE_WedPLF2g0LG8DmYD6aeg', :content_type => :json}
     end
-
-    @response = RestClient.get 'https://api.clashofclans.com/v1/clans/%23'+ @input, {:Authorization => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjFhMTYwY2Y1LTBmNDMtNDNkOS1iOWQ2LTgwYWQ5Y2RlYWYxNSIsImlhdCI6MTUwODE5MTkzOSwic3ViIjoiZGV2ZWxvcGVyLzBmODMwMDgxLTAwZDUtYzdkMC1iODVjLTAxMWFmYzcyZmE2MSIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjU0LjY5LjU3LjE5MCJdLCJ0eXBlIjoiY2xpZW50In1dfQ.Devza1EMm4hZnZbWQ3RGFwd-wjLrF04vQYzWMr75QkBelOOcVYBRxMklQxlkRp6VBjzNSyAICM0gUV_yBK8x1g', :content_type => :json}
 
     @clan = JSON.parse(@response)
 
