01169cd0a7e77385b396455b7b20b0428b2d9402
JasonLazzuri <jason.Lazzuri@gmail.com>
2018-04-22 15:44:47 -0700

    each user can only see their own account page

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -5,7 +5,15 @@ class AccountsController < ApplicationController
   end
 
   def show
-
+      if current_user.nil?
+        flash[:notice] = 'You need to login first!'
+        redirect_to new_user_session_path
+      elsif current_user.id != params[:id].to_i
+        flash[:notice] = 'You do not have access to this page!'
+        redirect_to account_path(current_user.id)
+      else
+        account_path(current_user.id)
+      end
   end
 
   private
